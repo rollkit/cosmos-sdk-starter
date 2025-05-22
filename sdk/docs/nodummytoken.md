2f4ffe6602f90d3dac5c8df29fe17a337f7adaea
julienrbrt <julien@rbrt.fr>
2025-05-22 13:11:04 +0200

    refactor!: delete sdk package
    Remove SDK packages, those modules weren't production ready, so they shouldn't be present in this repo.
    They aren't maintained here as well.
    
    We are migrating, testing and improving them for Rollkit v1. The work is being tracked in https://github.com/rollkit/go-execution-abci/issues/102.

--- sdk/docs/nodummytoken.md
+++
@@ -1,3 +0,0 @@
-# No Dummy Token
-
-TBD
--- sdk/x/staking/keeper/delegate.go
+++
@@ -1,6 +0,0 @@
-package keeper
-
-import "github.com/cosmos/cosmos-sdk/x/staking/types"
-
-// Implements DelegationSet interface
-var _ types.DelegationSet = Keeper{}
